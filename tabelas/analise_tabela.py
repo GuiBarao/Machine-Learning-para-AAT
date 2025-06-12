846285838da8cc1b5c218f8f20967f7686e8a789
GuiBarao <guibarao04@gmail.com>
2025-06-12 08:55:06 -0400

    Criando gráficos das tabelas

--- tabelas/analise_tabela.py
+++ tabelas/analise_tabela.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from tabela_comparativa import lista_avaliacoes
 
 
@@ -10,11 +11,40 @@ def medias(corpus):
 
     return (array_humanas.mean(), array_sistema.mean())
 
+def cria_graficos(corpus):
+    humanas, sistema = lista_avaliacoes(corpus)
+
+    labels = [str(i) for i in range(len(humanas))]
+
+    map_corpus = {"kaggle" : "Ensino Fundamental", "uol":"Ensino Médio"}
+
+
+    plt.bar(labels, humanas)
+    plt.title(f'Avaliações de textos do {map_corpus[corpus]} dada por humanos')
+    plt.xlabel('Indice da avaliação')
+    plt.ylabel('Avaliação recebida')
+    plt.xticks(np.arange(0, len(labels), 10), rotation=90, fontsize=6)    
+    plt.tight_layout()
+    plt.savefig(f"tabelas\\graficos\\{corpus}\\humanas.pdf", format="pdf" )
+
+    plt.close()
+
+    plt.bar(labels, sistema)
+    plt.title(f'Avaliações de textos do {map_corpus[corpus]} dada pela sistema')
+    plt.xlabel('Indice da avaliação')
+    plt.ylabel('Avaliação recebida')
+    plt.xticks(np.arange(0, len(labels), 10), rotation=90, fontsize=6)    
+    plt.tight_layout()
+    plt.savefig(f"tabelas\\graficos\\{corpus}\\sistema.pdf", format="pdf" )
+
+    plt.close()
+
 def main():
     #print(medias("uol"))    #humana = 480.59149722735674 | sistema = 467.13863216266174
-
     #print(medias("kaggle")) #humana = 502.7734627831715  | sistema = 505.0129449838188
 
+    #cria_graficos("kaggle")
+    #cria_graficos("uol")
     pass
 
 
